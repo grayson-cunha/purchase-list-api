cca07f06a238a31feeac997c20ebbaaf3c5d7587
Grayson Cunha <graysonsilva93@gmail.com>
2022-11-19 15:24:07 -0300

    refactor: move general config to different module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { Product } from './products/product.entity';
+import { ConfigurationModule } from './configuration/configuration.module';
 import { ProductsModule } from './products/products.module';
-import { PurchaseList } from './purchase-list/purchase-list.entity';
 import { PurchaseListModule } from './purchase-list/purchase-list.module';
-import { User } from './users/user.entity';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [Product, PurchaseList, User],
-        synchronize: true,
-      }),
-    }),
-    ProductsModule,
-    PurchaseListModule,
-    AuthModule,
-  ],
+  imports: [ProductsModule, PurchaseListModule, AuthModule, ConfigurationModule],
   controllers: [AppController],
 })
 export class AppModule {}
---
+++ src/configuration/configuration.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Product } from '../products/product.entity';
+import { PurchaseList } from '../purchase-list/purchase-list.entity';
+import { User } from '../users/user.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Product, PurchaseList, User],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class ConfigurationModule {}
