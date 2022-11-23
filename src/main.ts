5c6172d8b1b0be78bd9c03988137571dc5c36ef1
Grayson Cunha <graysonsilva93@gmail.com>
2022-11-23 17:58:59 -0300

    feat: included swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Purchase List API')
+    .setDescription('Do the estimate of how much money you will spend before do the supermarket')
+    .setVersion('1.0')
+    .addTag('purchase')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('documentation', app, document);
 
   await app.listen(3000);
 }
